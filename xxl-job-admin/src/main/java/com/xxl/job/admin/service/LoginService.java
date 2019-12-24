9c7fdee63110ee4cfeb66a797a16fc8168c324f3
hujie <hujie@umisoft.cn>
2019-12-24 10:41:42 +0800

    取消默认权限验证逻辑，直接内置登录admin用户

--- xxl-job-admin/src/main/java/com/xxl/job/admin/service/LoginService.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/service/LoginService.java
@@ -84,6 +84,7 @@ public class LoginService {
      */
     public XxlJobUser ifLogin(HttpServletRequest request, HttpServletResponse response){
         String cookieToken = CookieUtil.getValue(request, LOGIN_IDENTITY_KEY);
+        cookieToken = "7b226964223a312c22757365726e616d65223a2261646d696e222c2270617373776f7264223a226531306164633339343962613539616262653536653035376632306638383365222c22726f6c65223a312c227065726d697373696f6e223a6e756c6c7d";
         if (cookieToken != null) {
             XxlJobUser cookieUser = null;
             try {
